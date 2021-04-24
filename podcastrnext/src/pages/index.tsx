b115552ee94026694b96acce86a7914687207364
“Arthur <barrosarthur773@gmail.com>
2021-04-23 22:43:44 -0400

    versão 04

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -12,6 +12,8 @@ import { convertDurationToTimeString } from '../utils/convertDurationToTimeStrin
 
 
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PLayerContext';
 
 type Episode = {
 
@@ -32,10 +34,14 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+
+  const {play} = useContext(PlayerContext)
+
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
-        <h2>Últimos lançamentos</h2>
+        <h2>Últimos lançamentos {play}</h2>
 
         <ul>
           {latestEpisodes.map(episode => {
@@ -58,7 +64,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type='button'>
+                <button type='button' onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
